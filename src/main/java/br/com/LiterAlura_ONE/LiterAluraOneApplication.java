db3be947433c3cd4463f93ad8db42d7a3447b363
PHmore <patryckhenryck401@gmail.com>
2025-08-19 15:20:54 -0300

    Primeiro commit

---
+++ src/main/java/br/com/LiterAlura_ONE/LiterAluraOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.LiterAlura_ONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/LiterAlura_ONE/LiterAluraOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LiterAlura_ONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
